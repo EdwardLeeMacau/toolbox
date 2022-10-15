9a53f0429d7a71623c9226fd3a08501a17413c4b
Edward Lee <34258587+EdwardLeeMacau@users.noreply.github.com>
2022-10-15 12:16:16 +0800

    Update download-package.sh

--- download-package.sh
+++ download-package.sh
@@ -4,3 +4,8 @@ apt-get install vim -y
 apt-get install git -y
 apt-get install tmux -y
 apt-get install tree -y
+apt-get install gcc g++ -y
+apt-get install make -y
+apt-get install cmake -y
+# Follow the setup guide in PacketCloud to configure `apt`
+# before `sudo apt-get install git-lfs`
