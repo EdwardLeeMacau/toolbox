454b71fe1e92039ffd7063cae68170b05f8b0cb2
Edward Lee <34258587+EdwardLeeMacau@users.noreply.github.com>
2023-11-28 12:15:23 +0800

    Update download-package.sh

--- download-package.sh
+++ download-package.sh
@@ -7,5 +7,16 @@ apt-get install tree -y
 apt-get install gcc g++ -y
 apt-get install make -y
 apt-get install cmake -y
+
 # Follow the setup guide in PacketCloud to configure `apt`
 # before `sudo apt-get install git-lfs`
+
+# oh-my-zsh installer, include several core plugins
+# 1. installer
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#
+# 2. plugin: zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#
+# 3. plugin: zsh-syntax-highlighting (MacOS)
+# brew install zsh-syntax-highlighting
