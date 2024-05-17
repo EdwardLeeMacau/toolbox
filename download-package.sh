0821c90950ac67bd8a7eb3dfa315be7fac2269da
Edward Lee <34258587+EdwardLeeMacau@users.noreply.github.com>
2024-05-17 23:18:36 +0800

    Update download-package.sh

--- download-package.sh
+++ download-package.sh
@@ -7,6 +7,7 @@ apt-get install tree -y
 apt-get install gcc g++ -y
 apt-get install make -y
 apt-get install cmake -y
+apt-get install net-tools
 
 # Follow the setup guide in PacketCloud to configure `apt`
 # before `sudo apt-get install git-lfs`
